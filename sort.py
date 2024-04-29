a1d3df370cc30ae0b1ab0b21ea249cd8ee47531c
Hovhannisyan111 <ogannisian.2004.01@gmail.com>
2024-04-29 12:24:01 +0400

    Sorting files

--- sort.py
+++ sort.py
@@ -12,9 +12,8 @@ def get_argument():
 def sort_files(fname):
     for i in os.listdir(fname):
         f_path = os.path.join(fname, i)
-        if os.path.isfile(f_path):
-            file_type = i.split(".")[-1]
-            ddir = os.path.join(fname, file_type)
+        file_type = i.split(".")[-1]
+        ddir = os.path.join(fname, file_type)
         if not os.path.exists(ddir):
             os.mkdir(ddir)
         shutil.move(f_path, ddir)
