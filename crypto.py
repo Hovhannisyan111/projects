2ef0eb383ce9f65f1f91d6ecc497dfd5fd9795a9
Hovhannisyan111 <ogannisian.2004.01@gmail.com>
2024-05-01 12:36:21 +0400

    Crypto

--- crypto.py
+++ crypto.py
@@ -1,21 +1,52 @@
+"""
+This file is for showing Crypto currence datas
+Created by: Arman Hovhannisyan
+Date: 30.04.2024
+"""
+
 import requests
 import json
 import time
 
 def get_data():
+    """
+    Function: get_data
+    Brief: The function sends a request to this api 
+    Return: returns list of dictionaries conataining recieved data
+    """
     url = "https://api.coincap.io/v2/assets"
-    params = {"limit": 20}
-    response = requests.get(url, params=params)
-    datas = response.json()
-    return datas['data']
+    params = {"limit": 20}    
+    try:
+        response = requests.get(url, params=params)
+        datas = response.json()
+        if response.status_code == 200:
+            return datas['data']
+    except Exception as e:
+        print(e)
+    exit()
 
 def filter_name(datas, name):
+    """
+    Function: filter_name
+    Params: datas, name: name of the input
+    Return: returns filterd list of dicts
+    """
     return [i for i in datas if name.lower() in i["name"].lower()]
 
 def filter_value(datas, value):
+    """
+    Function: filter_value
+    Params: datas, value: inputed value to sort
+    Resturn: filtered list of dictionaries
+    """
     return [i for i in datas if float(i["priceUsd"]) > value]
 
 def output_data(datas):
+    """
+    Function: output_data
+    Brief: The funtion prints required data
+    Params: datas
+    """
     for data in datas:
         name = data['name']
         symbol = data['symbol']
@@ -32,18 +63,23 @@ def output_data(datas):
         print("------------------------------------")
 
 def main():
+    """
+    Function: main
+    Brief: Entery point
+    """
     while True:
-        datas = get_data()
-        output_data(datas)
-        name = input("Enter a name to find: ")
-        if name:
-            data = filter_name(datas, name)
-            output_data(data)
-        value = input("Enter a value to sort by: ")
-        if value.isdigit():
-            data = filter_value(datas, float(value))
-            output_data(data)
-        time.sleep(5)
+       datas = get_data()
+       output_data(datas)
+       name = input("Enter a name to find: ")
+       if name:
+           data = filter_name(datas, name)
+           output_data(data)
+       value = input("Enter a value to sort by: ")
+       if value.isdigit():
+           data = filter_value(datas, float(value))
+           output_data(data)
+       time.sleep(5)
+        
     
 if __name__ == "__main__":
     main()
