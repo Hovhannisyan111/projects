63100682bb79fc0947d28fdd743eb645162589a9
Hovhannisyan111 <ogannisian.2004.01@gmail.com>
2024-05-02 09:19:12 +0400

    Counting words

--- count.py
+++ count.py
@@ -1,32 +1,59 @@
+"""
+This file is countiong words in given txt file
+Created by: Arman Hovhannisyan
+Date: 29 april 2024
+"""
 import re
 
 def get_content(fname):
+    """
+    Function: get_content
+    Params: given txt file name
+    Brief: opening file, reading and removing non aplphanumeric characters
+    Return: returns a list of strings
+    """
     with open(fname) as f:
         ket = r"[^\w\s]"
         return re.sub(ket, "", f.read()).split()
 
-def count(content):
-    md = {}
-    for i in content:
-        if i.isalpha():
-            if i in md:
-                md[i] += 1
+def count_words(content):
+    """
+    Function: count_words
+    Params: content: a list of strings
+    Brief: counting each word
+    return: returns a dictionary of each word how many times used
+    """
+    word_count = {}
+    for word in content:
+        if word.isalpha():
+            if word in word_count:
+                word_count[word] += 1
             else:
-                md[i] = 1
-    return md
+                word_count[word] = 1
+    return word_count
 
 
-def show_data(ml):
-    for i in ml:
+def show_data(sorted_list):
+    """
+    Function: show_data
+    Params: sorted_list
+    Brief: printing sorted_list
+    """
+    for i in sorted_list:
         print(i[0],"-",i[1])
 
 
 def main():
+    """
+    Function: main
+    Brief: Entery point
+    """
     fname = "a.txt"
     content = get_content(fname)
-    md = count(content)
-    ml = sorted(md.items(), key=lambda x: x[1], reverse=True)
-    show_data(ml)
+    word_count = count_words(content)
+    sorted_list = sorted(word_count.items(), key=lambda x: x[1], reverse=True)
+    show_data(sorted_list)
 
 if __name__ == "__main__":
     main()
+
