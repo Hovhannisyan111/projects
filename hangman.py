ba89a630057fbd5630e0f36a2bd22c44cf65e6c4
Hovhannisyan111 <ogannisian.2004.01@gmail.com>
2024-05-02 19:38:42 +0400

    Hangman

--- hangman.py
+++ hangman.py
@@ -1,6 +1,16 @@
+"""
+This file is for hangman game
+Ceated by: Arman Hovhannisyan
+Date 29 april 2024
+"""
 def game(word, secret):
-    count = 7
-    while "-" in secret and count > -1:
+    """
+    Function: game
+    Params: word: The word  to guess, secret: coded type of the word 
+    Brief: prints the final staate of the word after the game
+    """
+    count = -1
+    while "-" in secret and count < 6:
         print(secret)
         letter = input("Enter a letter: ").lower()
         
@@ -10,23 +20,103 @@ def game(word, secret):
                     if letter == word[i]:
                         secret = secret[:i] + letter + secret[i+1:]
             else:
-                print(f"Incorrect. You got {count} try")
-                count -= 1
+                print(f"Incorrect. Try again")
+                count += 1
+                if count < 6:
+                    hangman(count)
         else:
             print("Invalid input!")
-    return secret
-
-def check(secret, word):
-    if "-" not in secret:
-        print("You Won.", word)
+    if count == 6:
+        hangman(count)
+        print("Game over. Word was:", word)
     else:
-        print("You lose. Word was", word)
+        print("You win:", word) 
+
+
+def hangman(count):
+    """
+    Function: hangman
+    Params: count: The count of wrong answers
+    Brief: Showing pictures of hangman
+    """
+    pictures = [
+        """
+        ------
+        |    |
+        |
+        |
+        |
+        |
+        -
+        """,
+        """
+        ------
+        |    |
+        |    O
+        |
+        |
+        |
+        -
+        """,
+        """
+        ------
+        |    |
+        |    O
+        |    |
+        |
+        |
+        -
+        """,
+        """
+        ------
+        |    |
+        |    O
+        |   /|
+        |
+        |
+        -
+        """,
+        """
+        ------
+        |    |
+        |    O
+        |   /|\\
+        |
+        |
+        -
+        """,
+        """
+        ------
+        |    |
+        |    O
+        |   /|\\
+        |   /
+        |
+        -
+        """,
+        """
+        ------
+        |    |
+        |    O
+        |   /|\\
+        |   / \\
+        |
+        -
+        """
+    ]
+    
+    print(pictures[count])
+
 
 def main():
+    """
+    qunction: main
+    Brief: Entery point
+    """
     word = "something"
     secret = "-" * len(word)
-    secret = game(word, secret)
-    check(secret, word)
+    game(word, secret)
+    
 
 if __name__ == "__main__":
     main()
